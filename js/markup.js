c76e0fe61a6c9f57a04693a3992e7c088f4fb03b
VorontsovIE <vorontsov.i.e@gmail.com>
2016-05-21 11:12:31 +0300

    Плашка для ввода сиквенса; Заглушка для клика кнопки; Выбор мотивов; Заглушка вместо реального вызова Ajax

--- js/markup.js
+++ js/markup.js
@@ -5,13 +5,27 @@ $(function(){
       callback(data['pwm']);
     });
   };
-  window.with_motif_pwm = with_motif_pwm;
 
-  $.get('http://hocomoco.autosome.ru/human/mono.json', function(data){
-    var motif_list = data;
-    var motif_list_formatted = $.map(motif_list, function(el,ind){
-      return '<div class="motif">'+ el +'</div>';
-    }).join('');
-    $('#motif-list').html(motif_list_formatted);
-  })
+  // $.get('http://hocomoco.autosome.ru/human/mono.json', function(data){
+  //   var motif_list = data;
+  //   var motif_list_formatted = $.map(motif_list, function(el,ind){
+  //     return '<div class="motif">'+ el +'</div>';
+  //   }).join('');
+  //   $('#motif-list').html(motif_list_formatted);
+  // });
+
+  // TODO: replace this stub with actual Ajax request (see above)
+  var motif_list_formatted = $.map(["AHR_HUMAN.H10MO.B","AIRE_HUMAN.H10MO.C","ALX1_HUMAN.H10MO.B"], function(el,ind){
+    return '<div class="motif">'+ el +'</div>';
+  }).join('');
+  $('#motif-list').html(motif_list_formatted);
+
+  $('#markup_button').click(function(event){
+    var sequence = $('#sequence_input').val();
+    console.log('Markup sequence ' + sequence);
+  });
+
+  $('#motif-list').on('click', '.motif', function(event){
+    $(event.target).toggleClass('selected');
+  });
 });
