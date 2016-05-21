5de6c2af1f3c62a06f4fae030baa1706e04ee2e0
VorontsovIE <vorontsov.i.e@gmail.com>
2016-05-21 10:13:11 +0300

    Загрузка одной PWM-ки

--- js/markup.js
+++ js/markup.js
@@ -1,4 +1,12 @@
 $(function(){
+  var with_motif_pwm = function(motif_name, callback) {
+    var result = null;
+    $.get('http://hocomoco.autosome.ru/motif/' + motif_name + '.json', {with_matrices: true}, function(data){
+      callback(data['pwm']);
+    });
+  };
+  window.with_motif_pwm = with_motif_pwm;
+
   $.get('http://hocomoco.autosome.ru/human/mono.json', function(data){
     var motif_list = data;
     var motif_list_formatted = $.map(motif_list, function(el,ind){
