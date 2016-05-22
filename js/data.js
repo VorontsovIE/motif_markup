70e8cd75fe85b14c68c4fe95c47bc6f509ef4379
Aleksandr Sher <aleksandrsher@gmail.com>
2016-05-22 12:04:29 +0300

    Версия с одним датасетом; нужно разбить на уровни

--- js/data.js
+++ js/data.js
@@ -1,3 +1,18 @@
-var sample = [{ "motif": "AHR_HUMAN.H10MO.B", "pos": 0, "length": 9, "strength": 0.000003814697265625, "strand": "+" }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 1, "length": 9, "strength": 0.000003814697265625, "strand": "+" }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 2, "length": 9, "strength": 0.000003814697265625, "strand": "+" }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 3, "length": 9, "strength": 0.000003814697265625, "strand": "+" }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 4, "length": 9, "strength": 0.000003814697265625, "strand": "+" }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 5, "length": 9, "strength": 0.000003814697265625, "strand": "+" }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 6, "length": 9, "strength": 0.000003814697265625, "strand": "+" }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 7, "length": 9, "strength": 0.000003814697265625, "strand": "+" }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 8, "length": 9, "strength": 0.000003814697265625, "strand": "+" }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 9, "length": 9, "strength": 0.000003814697265625, "strand": "+" }]
+var sequence = 'AAAGTGCTGCTGAGGCGTAGAGCGTCGGCTGATGCGCTTGACTAGACTAACGTTA';
+var sequenceArray = _.map(sequence, function(x) { return x; });
 
-var data = Array.apply(null, Array(10)).map(function (_, i) { return i; });
+function log10 (x){ 
+	return Math.log(x) / Math.log(10);
+}
+
+var hitsSample = [{ "motif": "AHR_HUMAN.H10MO.B", "pos": 13, "length": 9, "strength": 0.020551430419662436, "strand": "+", "depth": 0 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 16, "length": 9, "strength": 0.03081743317182391, "strand": "+", "depth": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 18, "length": 9, "strength": 0.013724867266903848, "strand": "+", "depth": 3 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 23, "length": 9, "strength": 0.020551430419662436, "strand": "+", "depth": 3 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 26, "length": 9, "strength": 0.06939833710167047, "strand": "+", "depth": 0 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 30, "length": 9, "strength": 0.04627956449367295, "strand": "+", "depth": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 32, "length": 9, "strength": 0.006091535254410528, "strand": "+", "depth": 3 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 46, "length": 9, "strength": 0.04627956449367295, "strand": "+", "depth": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 2, "length": 9, "strength": 0.009150949092665239, "strand": "-", "depth": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 18, "length": 9, "strength": 0.009150949092665239, "strand": "-", "depth": 0 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 22, "length": 9, "strength": 0.013724867266903848, "strand": "-", "depth": 1 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 30, "length": 9, "strength": 0.006091535254410528, "strand": "-", "depth": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 32, "length": 9, "strength": 0.020551430419662436, "strand": "-", "depth": 0 }];
+var hits = _.chain(hitsSample)
+	.sortBy(function (x) { return x.pos; })
+	.map(function (x) { return Object.assign(x, { strength: -log10(x.strength) }); })
+	//.groupBy("strength")
+	.value();
+
+	var data = {
+	sequence: sequence,
+	hits: hits
+};
--- js/visualize.js
+++ js/visualize.js
@@ -1,6 +1,35 @@
-d3.select(".chart")
+// constants
+
+var LETTER_WIDTH_PX = 31.39;
+var HIT_HEIGHT_PX = 30;
+
+var STRENGTH_MIN = 1;
+var STRENGTH_MAX = 3;
+
+var HIT_HEIGHT_MIN_PX = 10;
+var HIT_HEIGHT_MAX_PX = 30;
+
+// d3
+
+d3.select(".sequence-index")
 	.selectAll("div")
-		.data(data)
-	.enter().append("div")
-		.style("width", function (d) { return d * 10 + "px"; })
+		.data(sequenceArray)
+	.enter().append("span")
+		.text(function (d, i) { return i; });
+
+d3.select(".sequence")
+	.selectAll("div")
+		.data(sequenceArray)
+	.enter().append("span")
 		.text(function (d) { return d; });
+
+d3.select(".hits")
+	.selectAll("div")
+		.data(hits)
+	.enter().append("div")
+		.attr("class", "hit")
+		.style("width", function (d) { return d.length * LETTER_WIDTH_PX + "px"; })
+		.style("height", HIT_HEIGHT_PX + "px")
+		.style("margin-left", function (d) { return d.pos * LETTER_WIDTH_PX + "px"; })
+		.style("margin-top", function (d) { return d.depth * HIT_HEIGHT_PX + "px"; })
+		.text(function (d) { return d.motif; });
