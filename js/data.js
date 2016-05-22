b2621937f8a20cb94bd39930472ea55e7a99ebff
Aleksandr Sher <aleksandrsher@gmail.com>
2016-05-22 14:18:26 +0300

    Немного бутстрапчика + подсветка сайтов в сиквенсе

--- js/data.js
+++ js/data.js
@@ -1,15 +1,21 @@
-var sequence = 'AAAGTGCTGCTGAGGCGTAGAGCGTCGGCTGATGCGCTTGACTAGACTAACGTTA';
-var sequenceArray = _.map(sequence, function(x) { return x; });
+var sequenceSample = 'AAAGTGCTGCTGAGGCGTAGAGCGTCGGCTGATGCGCTTGACTAGACTAACGTTA';
+var sequenceArray = _.map(sequenceSample, function (x) { return x });
+var sequence = {
+	array: sequenceArray,
+	isHighlighted: false,
+	highlightFrom: -1,
+	highlightTo: -1
+}
 
 function log10 (x){ 
 	return Math.log(x) / Math.log(10);
 }
 
-var hitsSample = [{ "motif": "AHR_HUMAN.H10MO.B", "pos": 13, "length": 9, "strength": 0.020551430419662436, "strand": "+", "depth": 0 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 16, "length": 9, "strength": 0.03081743317182391, "strand": "+", "depth": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 18, "length": 9, "strength": 0.013724867266903848, "strand": "+", "depth": 3 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 23, "length": 9, "strength": 0.000002, "strand": "+", "depth": 3 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 26, "length": 9, "strength": 0.06939833710167047, "strand": "+", "depth": 0 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 30, "length": 9, "strength": 0.04627956449367295, "strand": "+", "depth": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 32, "length": 9, "strength": 0.006091535254410528, "strand": "+", "depth": 3 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 46, "length": 9, "strength": 0.04627956449367295, "strand": "+", "depth": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 2, "length": 9, "strength": 0.009150949092665239, "strand": "-", "depth": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 18, "length": 9, "strength": 0.009150949092665239, "strand": "-", "depth": 0 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 22, "length": 9, "strength": 0.013724867266903848, "strand": "-", "depth": 1 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 30, "length": 9, "strength": 0.006091535254410528, "strand": "-", "depth": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 32, "length": 9, "strength": 0.020551430419662436, "strand": "-", "depth": 0 }];
+var hitsSample = [{ "motif": "AHR_HUMAN.H10MO.B", "pos": 12, "length": 9, "strength": 1.1586509358307475, "strand": "+", "level": 0 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 29, "length": 9, "strength": 1.511203537121056, "strand": "+", "level": 0 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 29, "length": 9, "strength": 1.511203537121056, "strand": "-", "level": 1 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 33, "length": 9, "strength": 1.1586509358307475, "strand": "-", "level": 2 }, { "motif": "AHR_HUMAN.H10MO.B", "pos": 43, "length": 9, "strength": 1.1586509358307475, "strand": "+", "level": 0 }];
 var hits = _.chain(hitsSample)
 	.sortBy(function (x) { return x.pos; })
-	.map(function (x) { return Object.assign(x, { strength: -log10(x.strength) }); })
-	.groupBy("depth")
+	//.map(function (x) { return Object.assign(x, { strength: -log10(x.strength) }); })
+	.groupBy("level")
 	.map(function(value, key){ return { depth: key, hits: value}; })
 	.value();
 
--- js/visualize.js
+++ js/visualize.js
@@ -6,8 +6,8 @@ var HIT_HEIGHT_PX = 30;
 var STRENGTH_MIN = 1;
 var STRENGTH_MAX = 7;
 
-var HIT_HEIGHT_MIN_PX = 10;
-var HIT_HEIGHT_MAX_PX = 30;
+var HIT_HEIGHT_MIN_PX = 30;
+var HIT_HEIGHT_MAX_PX = 60;
 
 // d3
 
@@ -24,19 +24,42 @@ d3.select(".sequence-index")
 	.enter().append("span")
 		.text(function (d, i) { return i; });
 
-d3.select(".sequence")
-	.selectAll("div")
-		.data(sequenceArray)
+function highlightSequence(sequence, start, end) {
+	return Object.assign(sequence, { isHighlighted: true, highlightFrom: start, highlightTo: end });
+}
+
+function removeHighlight(sequence) {
+	return Object.assign(sequence, { isHighlighted: false, highlightFrom: -1, highlightTo: -1 });
+}
+
+function updateSequence(data) {
+	d3.select(".sequence")
+	.selectAll("span")
+		.data(data.array)
+	.attr("class", function (_, i) {
+		if (!data.isHighlighted)
+			return "";
+
+		return data.highlightFrom <= i && i <= data.highlightTo
+			? "m-highlighted"
+			: "";
+	})
 	.enter().append("span")
 		.text(function (d) { return d; });
+}
+
+function renderSequence(fn) {
+	data.sequence = fn(data.sequence);
+	updateSequence(data.sequence);
+}
 
 d3.select(".hits")
 	.selectAll("div")
-		.data(hits)
+		.data(data.hits)
 	.enter().append("div")
 		.attr("class", "hits-row")
 		.style("height", function (d) {
-		var maxStrength = d3.max(d.hits, function(h) { return h.strength; });
+			var maxStrength = d3.max(d.hits, function (h) { return h.strength; });
 			return strengthScale(maxStrength) + "px";
 		})
 		.selectAll("div")
@@ -46,4 +69,9 @@ d3.select(".hits")
 			.style("width", function (d) { return d.length * LETTER_WIDTH_PX + "px"; })
 			.style("height", function (d) { return strengthScale(d.strength) + "px"; })
 			.style("margin-left", function (d) { return d.pos * LETTER_WIDTH_PX + "px"; })
-			.text(function (d) { return d.motif; });
+			.text(function (d) { return d.motif; })
+			.on("mouseover", function (d) { renderSequence(function(s) { return highlightSequence(s, d.pos, d.pos + d.length - 1); }); })
+			.on("mouseout", function (d) { renderSequence(function (s) { return removeHighlight(s); }); })
+//.on("mousemove", function (d) { console.log('mouse move on ' + d.pos + ' to ' + d.pos + d.length - 1); });
+
+updateSequence(data.sequence);
